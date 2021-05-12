8af5a3d1d696d2e939be479d208f101998e26f33
Arash <arash.ahmadii1378@gmail.com>
2021-05-13 03:48:53 +0430

    Change click to pressing enter to play steins gate

--- Syncromote/MainWindow.xaml.cs
+++ Syncromote/MainWindow.xaml.cs
@@ -11,6 +11,7 @@ using System.Timers;
 using NHotkey;
 using NHotkey.Wpf;
 using System.Windows.Media;
+using System.Threading.Tasks;
 
 namespace Syncromote
 {
@@ -287,8 +288,10 @@ namespace Syncromote
                     int x1 = Int32.Parse(result[0]);
                     int y1 = Int32.Parse(result[1]);
                     MouseOperations.SetCursorPosition(x1, y1);
-                    MouseOperations.MouseEvent(MouseOperations.MouseEventFlags.LeftDown);
-                    MouseOperations.MouseEvent(MouseOperations.MouseEventFlags.LeftUp);
+                    //MouseOperations.MouseEvent(MouseOperations.MouseEventFlags.LeftDown);
+                    //MouseOperations.MouseEvent(MouseOperations.MouseEventFlags.LeftUp);
+                    //PerformAClick.CLICK();
+                    Keyboard.Send(Keyboard.ScanCodeShort.RETURN);
                 }
 
                 if (type == "t")
@@ -313,7 +316,12 @@ namespace Syncromote
 
         private void OnIncrement(object sender, HotkeyEventArgs e)
         {
-            
+            //Task.Delay(2000).ContinueWith(_ =>
+            //{
+            //    //PerformAClick.CLICK();
+            //    Keyboard.Send(Keyboard.ScanCodeShort.RETURN);
+            //}
+            //);
 
             if (isEstablished )
             {
@@ -646,5 +654,72 @@ namespace Syncromote
         }
     }
 }
+public class PerformAClick
+{
+    [DllImport("user32.dll")]
+    internal static extern uint SendInput(uint nInputs, [MarshalAs(UnmanagedType.LPArray), In] INPUT[] pInputs, int cbSize);
 
+    //main calling method
+    public static void CLICK()
+    {
+        INPUT[] i = new INPUT[1];
 
+        i[0].type = 0;
+        i[0].U.mi.time = 0;
+        i[0].U.mi.dwFlags = MOUSEEVENTF.LEFTDOWN | MOUSEEVENTF.LEFTDOWN;
+        i[0].U.mi.dwExtraInfo = UIntPtr.Zero;
+        i[0].U.mi.dx = 1;
+        i[0].U.mi.dy = 1;
+
+        SendInput(1, i, INPUT.Size);
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct INPUT
+    {
+        internal uint type;
+        internal InputUnion U;
+        internal static int Size
+        {
+            get { return Marshal.SizeOf(typeof(INPUT)); }
+        }
+    }
+
+    [StructLayout(LayoutKind.Explicit)]
+    internal struct InputUnion
+    {
+        [FieldOffset(0)]
+        internal MOUSEINPUT mi;
+
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    internal struct MOUSEINPUT
+    {
+        internal int dx;
+        internal int dy;
+        internal int mouseData;
+        internal MOUSEEVENTF dwFlags;
+        internal uint time;
+        internal UIntPtr dwExtraInfo;
+    }
+
+    [Flags]
+    internal enum MOUSEEVENTF : uint
+    {
+        ABSOLUTE = 0x8000,
+        HWHEEL = 0x01000,
+        MOVE = 0x0001,
+        MOVE_NOCOALESCE = 0x2000,
+        LEFTDOWN = 0x0002,
+        LEFTUP = 0x0004,
+        RIGHTDOWN = 0x0008,
+        RIGHTUP = 0x0010,
+        MIDDLEDOWN = 0x0020,
+        MIDDLEUP = 0x0040,
+        VIRTUALDESK = 0x4000,
+        WHEEL = 0x0800,
+        XDOWN = 0x0080,
+        XUP = 0x0100
+    }
+}
